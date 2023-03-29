92f91918e085f5133b1e2e6dc094116b7982c4f6
Fitri Rahim <fitrialrahim@gmail.com>
2023-03-30 00:10:12 +0800

    add comment on the usage how-to

--- record.sh
+++ record.sh
@@ -1,1 +1,4 @@
+#!/bin/bash
+
+# set env PROMPT_COMMAND to this file script
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] $(pwd) : $(history | sed 's/^\s*[0-9]\+\s\+//' | tail -n 2 | head -n 1)" >> ~/.history
