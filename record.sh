e4511a7d96c5d00fc66062415e5c80838c0616f9
Fitri Rahim <fitrialrahim@gmail.com>
2023-03-30 00:44:04 +0800

    removed comment due to conflicted with env

--- record.sh
+++ record.sh
@@ -1,4 +1,2 @@
-#!/bin/bash
-
-# set env PROMPT_COMMAND to this file script
 echo "[$(date '+%Y-%m-%d %H:%M:%S')] $(pwd) : $(history | sed 's/^\s*[0-9]\+\s\+//' | tail -n 2 | head -n 1)" >> ~/.history
+
